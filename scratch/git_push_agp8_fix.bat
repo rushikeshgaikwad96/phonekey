502a516c4598b7c1e500058f084d021e9448d5bc
PhoneKey Developer <developer@phonekey.org>
2026-08-14 19:12:44 +0530

    Fix AGP 8.0 manifest package collision and update Compose BOM

---
+++ scratch/git_push_agp8_fix.bat
@@ -0,0 +1,4 @@
+@echo off
+git add .
+git commit -m "Fix AGP 8.0 manifest package collision and update Compose BOM"
+git push origin main
---
+++ scratch/git_push_localprop.bat
@@ -0,0 +1,4 @@
+@echo off
+git add .github/workflows/android_build.yml
+git commit -m "Create local.properties and add stacktrace to android_build.yml"
+git push origin main
